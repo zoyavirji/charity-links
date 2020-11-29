5fa156ade82835748cca7ccff7d9fdbbb2688c93
zoyavirji <zoyav786@gmail.com>
2020-11-29 04:24:03 +0000

    Add some functionality to cause info

--- src/components/CauseInfo.vue
+++ src/components/CauseInfo.vue
@@ -1,6 +1,24 @@
 <template>
-  <v-card>
-    {{causeData.title}}
+  <v-card max-width="800" class="mx-auto">
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="causeData.imageUrl"
+    >
+    <v-card-title>{{causeData.title}}</v-card-title>
+    </v-img>
+    <v-card-text class="text--primary text-justify">
+      <div>{{causeData.description}}</div>
+      <div class="my-4">
+        <h3>Links:</h3>
+        <a v-for="link in causeData.links" :key="link.url" :href="link.url" target="_blank">
+        <v-btn
+            rounded
+            color="primary"
+            dark>{{link.name}}</v-btn>
+        </a>
+      </div>
+    </v-card-text>
   </v-card>
 </template>
 
