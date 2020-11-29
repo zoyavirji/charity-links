c11ec20e9bc6f79b0a3bb8844b3be882c2daea05
zoyavirji <zoyav786@gmail.com>
2020-11-29 04:10:50 +0000

    Update logo and fonts

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,18 @@
       prominent
     >
       <v-spacer></v-spacer>
+      <router-link to="/">
       <div class="d-flex justify-center">
         <v-img
             :src="require('./assets/logo.png')"
             width="100"
         ></v-img>
-        <div class="d-flex justify-center align-start flex-column logo mt-4 ml-2" color="#153B50">
-            <span class="text-h4 font-weight-light">CBAH</span>
-            <span class="text-subtitle-1">Charity Begins At Home</span>
+        <div class="d-flex justify-center align-start flex-column logo ml-2">
+            <span class="cyan--text text--darken-2 font-weight-bold logo">CBAH</span>
+            <span class="text-subtitle-1 cyan--text text--darken-4">Charity Begins At Home</span>
         </div>
       </div>
+      </router-link>
       <v-spacer></v-spacer>
       <a href="/about">About</a>
     </v-app-bar>
@@ -29,23 +31,17 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #153B50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.logo {
+    font-family: 'Quicksand', sans-serif;
+    font-size: 42px;
+    margin-bottom: -12px;
 }
+
 </style>
