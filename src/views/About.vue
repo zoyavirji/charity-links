ab22065dcb484c1396ce66688266a36b0c178e4b
zoyavirji <zoyav786@gmail.com>
2021-01-06 20:36:21 +0000

    added info to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+        Hi! My name is Zoya, I am a 16 year old high school student in Alberta. During the coronavirus lockdown, I have taken the time to expand my skills in coding while taking time to research current events. Combining both of these activities, I have decided to use these together to create a website. 
+    </div>
+    <div>
+        Due to all the atrocities that have taken place during 2020, I have decided to create a website which compiles some of the best donation links that support causes such as Black Lives Matter and the explosion in Lebanon. With this, I have included information blurbs about the topic to help spread awareness of these matters and to help you make an informed decision about which charity to donate to. 
+    </div>
+    <div>
+    As a teenager in today's society, I get the majority of my information from common social media platforms. Of course, the downside to this is that some critical information is not revealed or it is patently false. This website was made to serve the purpose of having easy access to ways the public can help with global issues and to educate and inform people of the background and relevance that aren’t spoken about on social media platforms and other popular news sources. Each blurb of information is short and concise to deliver the information as efficiently as possible.
+    </div>
   </div>
 </template>
