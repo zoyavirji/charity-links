25d13e80a1cb6a42078fdbb9fe44395baad227ba
zoyavirji <zoyav786@gmail.com>
2021-01-30 19:47:03 +0000

    update covid stats

--- src/assets/CauseData.js
+++ src/assets/CauseData.js
@@ -2,7 +2,7 @@ const COVID_19 = {
     id: "covid-19",
     title: "COVID-19",
     description: `COVID-19 originated in Wuhan, China. This virus has travelled around the world and has been declared by the world Health Organization 
-        as a worldwide pandemic. As of January 17, 2021, nearly 100 million positive cases have been detected with over 2 million deaths. The hardest hit country being America. 
+        as a worldwide pandemic. Over 100 million positive cases have been detected with over 2 million deaths. Many countries have been heavily affected, including America, Italy and Spain.
         Coronavirus is a family of viruses that can affect humans and animals. They’re structure is a sphere with spikes of protein surrounding it to 
         infect healthy cells. These spikes also allow the body to acknowledge its presence which then causes the symptoms to show. Because the form of 
         this virus replicated a crown, it is named "corona" meaning crown in Latin. It is said that the virus originated from a bat but this has not yet 
